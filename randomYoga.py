7ed3b5d3996492ca3f6e349fc72721d9aaf6a3d3
yijack <yijackyau1982@gmail.com>
2021-03-04 09:07:30 +0800

    Add files via upload

---
+++ randomYoga.py
@@ -0,0 +1,8 @@
+import random
+
+foo = ['Boat Pose', 'Lotus Pose', 'Diamond Pose', 'Half Spinal Twist Pose', 'Seated Fwd Bend', 'Plow Pose', 'Peacock Pose','Headstand','shoulder stand' , 'Bridge Pose']
+print ('Downward-facing Dog + ')
+print(random.choice(foo))
+
+## https://www.menshealth.com/fitness/g22739495/yoga-poses-for-men/ 
+## https://www.stylecraze.com/articles/effective-yoga-poses-to-increase-your-brain-power/
